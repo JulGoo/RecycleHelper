8692542542c1361bed593b127d62c1e73a983508
JulGoo <yrruda94@gmail.com>
2024-08-31 17:23:14 +0900

    firebase

--- app/src/main/java/kr/kro/barrierfree/recyclehelper/NoticeActivity.java
+++ app/src/main/java/kr/kro/barrierfree/recyclehelper/NoticeActivity.java
@@ -2,6 +2,7 @@ package kr.kro.barrierfree.recyclehelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,40 @@ public class NoticeActivity extends AppCompatActivity {
             return insets;
         });
 
+        //RecyclerView 초기화
+        recyclerView = findViewById(R.id.rv_notices);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        //Firestore 초기화
+        db = FirebaseFirestore.getInstance();
+
+        //RecyclerView에 어댑터 설정
+        noticeAdapter = new NoticeAdapter(noticeList);
+        recyclerView.setAdapter(noticeAdapter);
+
+        //Firestore에서 데이터 가져오기
+        loadNoticeFromFirestore();
+    }
+
+    private void loadNoticeFromFirestore() {
+        db.collection("notices")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        if (querySnapshot != null){
+                            noticeList.clear();
+                            for (QueryDocumentSnapshot document : querySnapshot) {
+                                Notice notice = document.toObject(Notice.class);
+                                noticeList.add(notice);
+                            }
+                            //어댑터에 데이터 변경 알림
+                            noticeAdapter.notifyDataSetChanged();
+                        }
+                    } else {
+                        //실패 시
+                        Log.e("NoticeActivity", "Error getting documents: ", task.getException());
+                    }
+                });
     }
 }
